aa457e8a137ee9315dbd1be0ffd53d31ced54d1a
tt-ph4t <tt.ph4t@gmail.com>
2026-03-08 13:15:22 +0700

    51

--- main/app/shared/hooks/use-memo.js
+++ main/app/shared/hooks/use-memo.js
@@ -1,30 +1,29 @@
-import { isUndefined } from '@sindresorhus/is'
 import { useCreation } from 'ahooks'
-import React from 'react'
 
 import { useDeepCompareMemoize } from './use-deep-compare-memoize'
 import { useRef } from './use-ref'
 
 const useCreation1 = useDeepCompareMemoize.with(useCreation)
-const useMemo1 = useDeepCompareMemoize.with(React.useMemo)
 
 export const useMemo = Object.assign(
-  (fn, deps) => {
-    const hasDeps = !isUndefined(deps)
-    const forceRecompute = deps === useMemo.forceRecompute
+  (fn, deps = useMemo.deps.default) => {
+    const isDefaultDeps = deps === useMemo.deps.default
 
     const value = (
-      forceRecompute
-        ? useMemo1
-        : hasDeps
-          ? useCreation1
-          : // https://garden.bradwoods.io/notes/javascript/react/memo-use-memo-use-callback
-            // If you need a reference to an object or array that doesn't require recalculation,
-            // useRef could be a better choice
-            useRef
-    )(fn, forceRecompute ? undefined : deps)
+      isDefaultDeps
+        ? // https://garden.bradwoods.io/notes/javascript/react/memo-use-memo-use-callback
+          // If you need a reference to an object or array that doesn't require recalculation,
+          // useRef could be a better choice
+          useRef
+        : useCreation1
+    )(fn, deps === useMemo.deps.noDeps ? undefined : deps)
 
-    return forceRecompute || hasDeps ? value : value.current
+    return isDefaultDeps ? value.current : value
   },
-  { forceRecompute: Symbol() }
+  {
+    deps: {
+      default: Symbol(),
+      noDeps: Symbol()
+    }
+  }
 )
