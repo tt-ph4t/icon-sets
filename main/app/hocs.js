7a97dea553fab62fd2c3fece877acbc2ad73a74b
tt-ph4t <tt.ph4t@gmail.com>
2026-06-09 13:36:21 +0700

    149

--- main/app/hocs.js
+++ main/app/hocs.js
@@ -1,10 +1,6 @@
 import {isEqual} from '@ver0/deep-equal'
 import React from 'react'
 
-import {useCallback} from './hooks/use-callback'
-
-useCallback // ?
-
 export const component = (Component = React.Fragment) =>
   React.memo(
     props => (
