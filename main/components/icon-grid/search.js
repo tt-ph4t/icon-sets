36e24f3cfab2be968eabe81cca0f8389b668e9c7
tt-ph4t <tt.ph4t@gmail.com>
2026-07-27 17:51:16 +0700

    209

--- main/components/icon-grid/search.js
+++ main/components/icon-grid/search.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 import {component} from '../../hocs'
 import {useCallback} from '../../hooks/use-callback'
+import {useCustomizedIcons} from '../../hooks/use-customized-icons'
 import {useEffect} from '../../hooks/use-effect'
 import {useRef} from '../../hooks/use-ref'
 import {isWordChar} from '../../misc'
@@ -12,19 +13,48 @@ import {Input} from '../input'
 import {Kbd} from '../kbd'
 import {Menu} from '../menu'
 import {Slot} from '../slot'
+import {ToolbarButton} from '../toolbar-button'
 import ColorPicker from './color-picker'
-import {SquareToggle} from './misc'
 import useStore from './use-store'
+
+const SquareToggle = component(props => {
+  const customizedIconsStore = useCustomizedIcons.useStore()
+
+  const defaultProps = customizedIconsStore.useSelectValue(({draft}) => ({
+    checked: draft.global.square
+  }))
+
+  return (
+    <Slot
+      onChange={event => {
+        customizedIconsStore.set(({draft}) => {
+          draft.global.square = event.target.checked
+        })
+      }}>
+      <ToolbarButton
+        {...defaultProps}
+        icon='symbol-ruler'
+        preventToggle
+        {...props}
+      />
+    </Slot>
+  )
+})
 
 const hotkeys = ['/', 'ctrl + f', 'ctrl + k', 'ctrl + e']
 
-const hotkeysMenu = [
-  'Hotkeys',
-  ...hotkeys.map(hotkey => ({
-    disabled: true,
-    label: Kbd.text(hotkey)
-  }))
-]
+const hotkeysMenu = (
+  <Menu
+    data={[
+      'Hotkeys',
+      ...hotkeys.map(hotkey => ({
+        disabled: true,
+        label: Kbd.text(hotkey)
+      }))
+    ]}
+    render={<VscodeIcon name='search' slot='content-before' />}
+  />
+)
 
 const Search = component(({children, ...props}) => {
   const ref = useRef()
@@ -64,10 +94,7 @@ const Search = component(({children, ...props}) => {
         value={searchTerm}
         {...props}>
         <React.Activity>
-          <Menu
-            data={hotkeysMenu}
-            render={<VscodeIcon name='search' slot='content-before' />}
-          />
+          {hotkeysMenu}
           {React.Children.map(children, children => (
             <div slot='content-after'>{children}</div>
           ))}
